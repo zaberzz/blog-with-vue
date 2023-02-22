2d1892034f85b60c306fd156d06d982506bf9086
Saba <saba.sadegh67@gmail.com>
2023-02-22 17:14:14 +0330

    creating single post component

---
+++ src/components/PostList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pos-list">
+    <div v-for="post in posts" :key="post.id"><SinglePost :post="post" /></div>
+  </div>
+</template>
+
+<script>
+import SinglePost from "./SinglePost.vue";
+
+export default {
+  props: ["posts"],
+  components: {
+    SinglePost,
+  },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>home</h1>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import PostList from "../components/PostList.vue";
+import { ref } from "vue";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      { title: "welcome to the blog", body: "lorem", id: 1 },
+      { title: "top 5 css trip", body: "lorem", id: 2 },
+    ]);
+    return { posts };
+  },
+};
 </script>
