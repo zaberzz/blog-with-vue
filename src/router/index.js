7077c7f20b74ec39f8bb4fb058c5fa5ac236fa23
Saba <saba.sadegh67@gmail.com>
2023-02-25 15:25:47 +0330

    creating new post view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import Details from "../views/Details.vue";
+import Create from "../views/Create.vue";
 
 const routes = [
   {
@@ -13,6 +14,7 @@ const routes = [
     name: "details",
     component: Details,
   },
+  { path: "/create", name: "create", component: Create },
 ];
 
 const router = createRouter({
